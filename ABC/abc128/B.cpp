d4eb53a18bcc62c25119116fc5aa837e3698f3fa
WTBacon <wtbacon514@gmail.com>
2019-05-26 23:57:30 +0900

    add abc128

---
+++ ABC/abc128/B.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <numeric>
+
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+
+    vector<int> I(N);
+    vector<string> S(N);
+    vector<int> P(N);
+
+    for (int i = 0; i < N; ++i) {
+        cin >> S[i] >> P[i];
+        I[i] = i + 1;
+    }
+
+    for (int n = 0; n< N -1 ; ++n) {
+        for (int i = 0; i < N - n - 1 ; ++i) {
+            if (S[i] > S[i + 1]) {
+                swap(S[i], S[i + 1]);
+                swap(P[i], P[i + 1]);
+                swap(I[i], I[i + 1]);
+            }
+            else if (S[i] == S[i + 1]) {
+                if (P[i] < P[i+1]) {
+                    swap(S[i], S[i + 1]);
+                    swap(P[i], P[i + 1]);
+                    swap(I[i], I[i + 1]);
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < N; ++i) cout << I[i] << endl;
+    return 0;
+}
---
+++ ABC/abc128/C.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <math.h>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N, M;
+    cin >> N >> M;
+
+    vector<vector<int>> k(M, vector<int>(N+1));
+    vector<int> p(M);
+
+    for (int m =0;m<M;++m) {
+        cin >> k[m][0];
+        for (int i = 0; i < k[m][0];++i) cin >> k[m][i+1];
+    }
+
+    for (int i =0; i<M;++i) cin >> p[i];
+
+    int total = 0;
+    for (int mask = 0; mask < (1 << N); ++mask){
+        bool flag = true;
+        for (int m = 0; m < M; ++m){
+            int tmpk = 0;
+            for (int i = 0; i < k[m][0]; ++i){
+                if ( mask & (1 << N - k[m][i+1])) {
+                    tmpk += 1;
+                }
+            }
+            if (tmpk % 2 != p[m]) flag = false;
+        }
+        if (flag) total += 1;
+    }
+
+    cout << total <<endl;
+
+    return 0;
+}
+

7857ed449c63f066c206fe8f43f71de2d8e082f9
WTBacon <wtbacon514@gmail.com>
2019-06-09 22:44:07 +0900

    abc129追加

---
+++ ABC/abc129/A.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int A, P;
+    cin >> A >> P;
+
+    cout << (3 * A + P) / 2 << endl;
+
+    return 0;
+}

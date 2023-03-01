759eb425449ddb860bd018c73c56011e881a1090
watabe-hiroaki <watabe_hiroaki@cyberagent.co.jp>
2023-03-01 21:58:09 +0900

    added T016

---
+++ TYPE90/T016.cpp
@@ -0,0 +1,167 @@
+#include <climits>
+#include <cmath>
+#include <iomanip>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
+//#include <bits/stdc++.h>
+
+#define rep(i, n) for (int (i) = 0; (i) < (n); ++(i))
+
+using namespace std;
+
+typedef long long ll;
+const long long INF = LLONG_MAX;
+const int MOD = pow(10, 9) + 7;
+const double pi = 3.14159265358979;
+
+// change minimum
+template<class T>
+void chmin(T &a, T b) {
+    if (a > b) {
+        a = b;
+    }
+}
+
+template<class T>
+bool chminBool(T &a, T b) {
+    if (a > b) {
+        a = b;
+        return true;
+    } else return false;
+}
+
+// change maximum
+template<class T>
+void chmax(T &a, T b) {
+    if (a < b) a = b;
+}
+
+ll gcd(ll a, ll b) {
+    if (b == 0) return a;
+    else return gcd(b, a % b);
+}
+
+ll lsm(ll a, ll b) {
+    // by GCD * LSM = a * b
+    return a / gcd(a, b) * b;
+}
+
+int binary_search() {
+    int left = 0, right = 0;
+
+    while (right - left > 1) {  // P(left)=false, P(right)=true より
+        int mid = (right - left) / 2;
+        if (mid) right = mid;
+        else left = mid;
+    }
+    return right;
+}
+
+struct Point {
+  double x, y;
+};
+
+Point operator-(const Point &p1, const Point &p2) {
+    return {p1.x - p2.x, p1.y - p2.y};
+}
+
+
+double getAngleFromB(Point p) {
+    double ang = atan2(p.y, p.x) * 180.0 / pi;
+    return ang < 0.0 ? 360.0 - abs(ang) : ang;
+}
+
+double getAngleByTwoPoints(double A, double C) {
+    // 原点と見立てた B からの偏角が A, C であることに注意
+    double diff = abs(A - C);
+    return diff >= 180.0 ? 360.0 - diff : diff;
+}
+
+// Union-Find
+struct UnionFind {
+  /**
+   * par: 各ノードの根ノードの番号(自分が根の場合は -1)
+   * siz: 各ノードの属する根付き木のノード数
+   */
+ private:
+  vector<int> par, siz;
+
+ public:
+  // UnionFind(int n) : par(n, -1), siz(n, 1) {}
+  void init(int n) {
+      par.resize(n, -1), siz.resize(n, 1);
+  }
+
+  // root を求める
+  int root(int x) {
+      if (par[x] == -1) return x;
+      else return par[x] = root(par[x]); // 経路圧縮
+  }
+
+  // x と y が同じグループか(root が同じか)
+  bool issame(int x, int y) {
+      return root(x) == root(y);
+  }
+
+  // x のグループと y のグループを併合する
+  bool unite(int x, int y) {
+      x = root(x);
+      y = root(y);
+
+      if (x == y) return false;
+
+      // Union by size
+      if (siz[x] < siz[y]) swap(x, y);
+
+      par[y] = x;
+      siz[x] += siz[y];
+      return true;
+  }
+
+  // x のグループのサイズ
+  int size(int x) {
+      return siz[root(x)];
+  }
+};
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    ll N;
+    cin >> N;
+
+    ll A, B, C;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    ll ans = 10000;
+    int M = 10000;
+    rep(a, M) {
+        rep (b, M - a) {
+            ll c = 0;
+            if (N - a * A - b * B > 0) c = (N - a * A - b * B) / C;
+            if (a * A + b * B + c * C == N) ans = min(ans, a + b + c);
+        }
+    }
+
+    cout << ans << endl;
+    return 0;
+}

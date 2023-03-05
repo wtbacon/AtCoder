d82ba060a7b536dbf9789efb873a8b2b8cb8d309
watabe-hiroaki <watabe_hiroaki@cyberagent.co.jp>
2023-03-05 17:24:35 +0900

    added atcoder library

--- TEMPLATES/DpTemplate.cpp
+++ TEMPLATES/DpTemplate.cpp
@@ -2,26 +2,12 @@
 #include <climits>
 #include <cmath>
 #include <iomanip>
-#include <iostream> // cout, endl, cin
-#include <string> // string, to_string, stoi
-#include <vector> // vector
-#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
-#include <utility> // pair, make_pair
-#include <tuple> // tuple, make_tuple
-#include <cstdint> // int64_t, int*_t
-#include <cstdio> // printf
-#include <map> // map
-#include <queue> // queue, priority_queue
-#include <set> // set
-#include <stack> // stack
-#include <deque> // deque
-#include <unordered_map> // unordered_map
-#include <unordered_set> // unordered_set
-#include <bitset> // bitset
-#include <cctype> // isupper, islower, isdigit, toupper, tolower
-//#include <bits/stdc++.h>
+#include <numeric>
+#include <bits/stdc++.h>
+#include <atcoder/all>
 
 using namespace std;
+using namespace atcoder;
 
 typedef long long ll;
 const long long INF = (1LL << 64) - 1L;
